90edaed96412507e40317e8069290e54db855f0f
Desmond Edem <edemintech@gmail.com>
2019-09-14 18:02:09 +0100

    chore(model): setup user model

--- src/database/models/users.js
+++ src/database/models/users.js
@@ -1,16 +1,76 @@
+import bcrypt from 'bcryptjs';
+import { config } from 'dotenv';
 import DB from '../index';
+
+config();
+const salt = process.env.SALT;
 
 /**
  * @class User
  */
 export default class User {
   /**
-   * 
+   * @param {object} user attributes
    */
-  all() {
+  constructor({
+    firstName, email, password, mobileNumber
+  }) {
+    this.id = User.incrementCount();
+    this.firstName = firstName;
+    this.email = email;
+    this.password = bcrypt.hashSync(password, salt);
+    this.mobileNumber = mobileNumber;
+    this.role = 'attendant';
+  }
+
+  /**
+   * @description increments attribute id
+   * @returns {id} attribute Id
+   * @memberof User
+   */
+  static incrementCount() {
+    User.count += 1;
+  }
+
+  /**
+   * @param {object} attributes
+   * @returns {object} user resource
+   * @memberof User
+   */
+  static create(attributes) {
+    const user = new User(attributes);
+    User.table.push(user);
+    return user;
+  }
+
+  /**
+   * @description - this method returns all objects
+   * @returns {object} user array
+   * @memberof User
+   */
+  static findAll() {
     return User.table;
+  }
+
+  /**
+   * @param {string} email
+   * @returns {object} user array
+   * @memberof User
+   */
+  static findOne(email) {
+    return User.table.find((user) => user.email === email);
+  }
+
+  /**
+   * @description resets user DB
+   * @returns {array} user array
+   * @memberof User
+   */
+  static resetTable() {
+    User.table = [];
+    User.count = 0;
   }
 }
 
-User.table = DB.user;
+User.table = DB.users;
 User.count = User.table.length;
